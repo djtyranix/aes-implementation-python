f1ba2582302ec787afa254935909cb71b6a5b9d8
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2021-10-12 11:38:37 +0700

    Added Time Test for library AES

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import time
 from aes import AESEncryption
 
 def main():
     msg = input("Enter a message: ")
+
+    start_encrypt_time = time.time()
     iv, ciphertext = AESEncryption.encrypt(msg)
+    end_encrypt_time = time.time()
+    encrypt_time = end_encrypt_time - start_encrypt_time
+
+    start_decrypt_time = time.time()
     decrypted = AESEncryption.decrypt(iv, ciphertext)
+    end_decrypt_time = time.time()
+    decrypt_time = end_decrypt_time - start_decrypt_time
 
     print(f"Encrypted = {ciphertext}")
-    print(f"Decrypted = {decrypted.decode('utf-8')}")
+    print(f"Decrypted = {decrypted}")
+    print(f"Encrypt time = {encrypt_time}")
+    print(f"Decrypt time = {decrypt_time}")
     
 
 if __name__ == "__main__":
